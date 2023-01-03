5f46c64a742c96f27e0a4e16bea0ef933b8093d0
baha-udin <mbaha713m@gmail.com>
2023-01-03 20:17:29 +0700

    Initial Commit

---
+++ swift-101/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-101
+//
+//  Created by Macbook on 03/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift-101/swift_101App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_101App.swift
+//  swift-101
+//
+//  Created by Macbook on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
